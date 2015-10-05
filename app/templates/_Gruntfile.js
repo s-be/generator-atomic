e97dc492a6cdd873fcb0b26af23c242548743eb4
Sebastian <sebastian.belter@nexum.de>
2015-10-05 16:08:35 +0200

    chore(develop branch): remove temp yaml integration

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -19,8 +19,6 @@
       var language = 'de'; }
     else {
       var language = grunt.option( 'language' ); }
-    // define a base language file
-    var langfile = grunt.file.readYAML('app/0_basics/'+ language +'.yaml');
 
     require('load-grunt-config')(grunt, {
       configPath: path.join(process.cwd(), 'tasks'),
