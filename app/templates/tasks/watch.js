db13578bae7d797346dd2cb4b8c1d16aa3c74fed
Aminata <aminata.sidibe@nexum.de>
2015-11-06 14:34:43 +0100

    feat(express-server): add express-server component

--- app/templates/tasks/watch.js
+++ app/templates/tasks/watch.js
@@ -4,60 +4,41 @@
  */
 
 module.exports = {
+  options: {
+  livereload: true
+  },
   server: {
-    options: {
-      livereload: true
-    },
     files: [
+      '<%= folders.tmp %>/{,*/,**/}*.html',
+      '<%= folders.tmp %>/{,*/}*.css',
       '<%= folders.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-    ]
+    ],
+    tasks: ['express']
   },
 
   javascript: {
-    options: {
-      livereload: true
-    },
     files: ['<%= folders.app %>/{,*/,**/}*.js'],
-    tasks: ['newer:babel' ]
-  },
-
-  javascripttest: {
-    options: {
-      livereload: false
-    },
-    files: ['<%= folders.tmp %>/{,*/,**/}*.js'],
-    tasks: ['newer:eslint', 'karma' ]
+    tasks: ['newer:babel', 'newer:eslint' ]
   },
 
   less: {
     options: {
-      livereload: true
+      livereload: false
     },
-    files: ['<%= folders.app %>/{,*/,**/}*.less'],
+    files: ['<%= folders.app %>/**/*.less'],
     tasks: ['less:server', 'autoprefixer']
   },
 
   jade: {
-    options: {
-      livereload: true
-    },
-    files: ['<%= folders.app %>/{,*/,**/}*.jade', '!**/_*'],
-    tasks: ['newer:jade:html']
+    files: ['<%= folders.app %>/{,*/,**/}*.jade', '!**/_*']
   },
 
   jadeincludes: {
-    options: {
-      livereload: true
-    },
     files: '<%= folders.app %>/{,*/,**/}_*.jade',
-    tasks: ['parallelize:jade']
+    tasks: ['concat:jadeincludes']
   },
 
   content: {
-    options: {
-      livereload: true
-    },
     files: '<%= folders.app %>/{,*/,**/}*.yaml',
-    tasks: ['parallelize:jade']
   }
 };
