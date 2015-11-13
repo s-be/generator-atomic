a0e99ba265f0ea7b6f93e77c8c6ed3f264292cf3
michaseel <seelmichael@gmail.com>
2015-11-13 10:52:04 +0100

    feat(less): output sourcemaps

--- app/templates/tasks/less.js
+++ app/templates/tasks/less.js
@@ -1,39 +1,62 @@
 /**
- * grunt-contrib-less options
+ * grunt-postcss options
  * @type {Object}
  */
 
 module.exports = {
+
   options: {
-    paths: ['<%= folders.app %>/'],
+    paths: ['<%= folders.tmp %>/'],
+    map: {
+      inline: false, // save all sourcemaps as separate files...
+      annotation: '.tmp/0_basics' // ...to the specified directory
+    }
   },
   dist: {
+    src: ['<%= folders.tmp %>/{,*/,**/}*.css'],
     options: {
-      compress: true,
-      cleancss: true,
-      sourceMap: true,
-      sourceMapURL: 'main.less.map',
-      sourceMapFilename: '<%= folders.dist %>/0_basics/main.less.map',
-      outputSourceFiles: true
-    },
-    files: [{
-      expand: true,
-      cwd: '<%= folders.app %>/0_basics',
-      src: ['*.less', '!**/_*', '!variables.less'],
-      dest: '<%= folders.tmp %>/0_basics',
-      ext: '.css'
-    }],
+      processors: [
+        require('autoprefixer')({
+          browsers: [
+            'Android 2.3',
+            'Android >= 4',
+            'Chrome >= 35',
+            'Firefox >= 31',
+            'Edge >= 12',
+            'Explorer >= 9',
+            'iOS >= 7',
+            'Opera >= 12',
+            'Safari >= 7.1'
+          ]
+        }),
+        require('cssnano')() // minify the result
+      ]
+    }
   },
   server: {
+    src: ['<%= folders.tmp %>/{,*/,**/}*.css'],
     options: {
-      dumpLineNumbers: 'all'
-    },
-    files: [{
-      expand: true,
-      cwd: '<%= folders.app %>/0_basics',
-      src: ['*.less', '!**/_*', '!variables.less'],
-      dest: '<%= folders.tmp %>/0_basics',
-      ext: '.css'
-    }],
+      processors: [
+        require('autoprefixer')({
+          browsers: ['last 2 version']
+        })
+      ]
+    }
+  },
+  stylelint: {
+    src: ['<%= folders.app %>/{,*/,**/}*.less', '!<%= folders.app %>/0_basics/{,*/,**/}*.less'],
+    options: {
+      writeDest: false,
+      map: false,
+      failOnError: true,
+      processors: [
+        require('stylelint')(),
+        require('postcss-reporter')({
+          clearMessages: true,
+          throwError: true,
+          noPlugin: true
+        })
+      ]
+    }
   }
 };
