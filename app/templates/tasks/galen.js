7391ffd1d88abb9d44136e258db3cd6355a0b66a
michaseel <seelmichael@gmail.com>
2015-11-18 14:21:43 +0100

    feat(galen): allow galen tests during development with grunt galen:server

--- app/templates/tasks/galen.js
+++ app/templates/tasks/galen.js
@@ -37,5 +37,39 @@ module.exports = {
         }
       }
     }
+  },
+  server: {
+    src: ['tests/galen.test.js'],
+    options: {
+      url: 'http://localhost:3000',
+      htmlReport: true,
+      htmlReportDest: 'report',
+      devices: {
+        desktop: {
+          deviceName: 'desktop',
+          browser: 'firefox',
+          size: '1280x800',
+          tags: ['all', 'lg']
+        },
+        tablet_landscape: {
+          deviceName: 'tablet-landscape',
+          browser: 'firefox',
+          size: '1024x768',
+          tags: ['all', 'md']
+        },
+        tablet_portrait: {
+          deviceName: 'tablet-portrait',
+          browser: 'firefox',
+          size: '768x1024',
+          tags: ['all', 'sm']
+        },
+        smartphone: {
+          deviceName: 'iphone6',
+          browser: 'firefox',
+          size: '375x667',
+          tags: ['all', 'xs']
+        }
+      }
+    }
   }
 };
